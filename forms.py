d04af62e911c0c2b3ee37b74b5405eaf0d23f0cc
EricaHD <ehd255@nyu.edu>
2021-08-29 19:30:24 -0400

    Adjust casing

--- forms.py
+++ forms.py
@@ -8,7 +8,7 @@ class RegistrationForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
     confirm_password = PasswordField("Confirm password", validators=[DataRequired(), EqualTo("password")])
-    submit = SubmitField("Sign up")
+    submit = SubmitField("Sign Up")
 
 
 class LoginForm(FlaskForm):
